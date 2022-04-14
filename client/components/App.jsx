bd690ee0640677d2b4451ac44f32aa42bc56459c
Joey Huntington <jwhunt19@gmail.com>
2022-04-13 23:08:16 -0600

    initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+
+
+  return (
+    <div>DigiList</div>
+  )
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path');
+
+const PORT = 3000;
+const app = express()
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at localhost:${PORT}!`);
+});
